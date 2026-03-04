9996bc3c6fb92c08e23ae1438800e6e82aa29257
Ankit Desai <ankit@pocketlabs.io>
2026-03-04 14:28:52 -0500

    Initial Commit

---
+++ NYCUnderground/NYCUndergroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCUndergroundApp.swift
+//  NYCUnderground
+//
+//  Created by Ankit Desai on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NYCUndergroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
